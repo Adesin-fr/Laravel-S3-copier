72c05b611500ac8fa70849a07b8dfae31e6fcd8e
Ludovic LEMARINEL <ludovic@lemarinel.net>
2021-11-11 21:58:54 +0100

    added some stats

--- app/Console/Commands/s3_copy.php
+++ app/Console/Commands/s3_copy.php
@@ -30,25 +30,32 @@ class s3_copy extends Command
     public function handle()
     {
 
+
         // List Files from source :
         $files = Storage::disk("source_s3")->allFiles();
-        $file_count =sizeof($files);
+        $file_count = sizeof($files);
         $current_file = 0;
+        $total_copied_size = 0;
 
-        echo  $file_count." files to copy !\n";
-        foreach($files as $file){
+        echo $file_count . " files to copy !\n";
+        $initial_start_time = time();
+        foreach ($files as $file) {
             $current_file++;
-            $start_time=microtime(true);
-            $file_contents =  Storage::disk("source_s3")->get($file);
-            $duration = microtime(true) - $start_time;
-            if ($duration){
-                $transfert_rate = number_format((strlen($file_contents)/1024) /$duration,0, ".", ""). "kB/s";
-            }else{
+            $start_time = microtime(true);
+            $file_contents = Storage::disk("source_s3")->get($file);
+            $file_size = strlen($file_contents) / 1024;
+            if ($duration) {
+                $transfert_rate = number_format($file_size / $duration, 0, ".", "") . "kB/s";
+            } else {
                 $transfert_rate = "Wow ! Too quick to count !";
             }
             Storage::disk("destination_s3")->put($file, $file_contents);
+            $duration = microtime(true) - $start_time;
+            $total_copied_size += $file_size;
             echo "Copied $file : $transfert_rate ($current_file / $file_count)\n";
         }
+        $total_duration = time() - $initial_start_time;
+        echo "All files done : $total_copied_size kB copied in $total_duration seconds (". number_format($total_copied_size / $total_duration, 0, ".", "") . "kB/s" .")" ;
 
         return Command::SUCCESS;
     }
