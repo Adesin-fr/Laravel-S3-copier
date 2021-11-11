88ff15e4b6132c678a15321c43ebd4fc2cff978e
Ludovic LEMARINEL <ludovic@lemarinel.net>
2021-11-11 21:42:35 +0100

    fix transfert rate division by zero

--- app/Console/Commands/s3_copy.php
+++ app/Console/Commands/s3_copy.php
@@ -41,9 +41,13 @@ class s3_copy extends Command
             $start_time=microtime(true);
             $file_contents =  Storage::disk("source_s3")->get($file);
             $duration = microtime(true) - $start_time;
-            $transfert_rate = number_format(strlen($file_contents/1024) /$duration,0, ".", "");
+            if ($duration){
+                $transfert_rate = number_format(strlen($file_contents/1024) /$duration,0, ".", ""). "kB/s";
+            }else{
+                $transfert_rate = "Wow ! Too quick to count !";
+            }
             Storage::disk("destination_s3")->put($file, $file_contents);
-            echo "Copied $file : $transfert_rate kB/s ($current_file / $file_count)\n";
+            echo "Copied $file : $transfert_rate ($current_file / $file_count)\n";
         }
 
         return Command::SUCCESS;
